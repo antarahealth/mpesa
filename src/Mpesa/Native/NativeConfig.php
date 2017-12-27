626a2c25e9b2130309b494709830cdc58bf44687
Julius Kabangi <kabangijulius@gmail.com>
2017-12-27 12:20:38 +0300

    Improve config resolver

--- src/Mpesa/Native/NativeConfig.php
+++ src/Mpesa/Native/NativeConfig.php
@@ -2,57 +2,169 @@
 
 namespace Kabangi\Mpesa\Native;
 
+use ArrayAccess;
 use Kabangi\Mpesa\Contracts\ConfigurationStore;
 
-/**
- * Class NativeConfig
- *
- * @category PHP
- *
- * @author   David Mjomba <Kabangiprivate@gmail.com>
- */
-class NativeConfig implements ConfigurationStore
+class NativeConfig implements ArrayAccess,ConfigurationStore
 {
-    //TODO: change implementation so user can enter the location.
-
     /**
-     * Mpesa configuration file.
+     * All of the configuration items.
      *
      * @var array
      */
-    protected $config;
+    protected $items = [];
 
     /**
-     * NativeConfig constructor.
+     * Create a new configuration repository.
+     *
+     * @param  array  $items
+     * @return void
      */
-    public function __construct()
-    {
+    public function __construct(){
         $defaultConfig = require __DIR__ . '/../../../config/mpesa.php';
         $userConfig    = __DIR__ . '/../../../../../../config/mpesa.php';
         $custom        = [];
         if (\is_file($userConfig)) {
             $custom = require $userConfig;
         }
-
-        $this->config = \array_merge($defaultConfig, $custom);
+        
+        $this->items = array_merge($defaultConfig, $custom);
     }
 
     /**
-     * Get the configuration value.
+     * Determine whether the given value is array accessible.
      *
-     * @param      $key
-     * @param null $default
-     *
-     * @return mixed|null
+     * @param  mixed  $value
+     * @return bool
      */
-    public function get($key, $default = null)
+    public static function accessible($value)
     {
-        $itemKey = \explode('.', $key)[1];
+         return is_array($value) || $value instanceof ArrayAccess;
+    }
 
-        if (isset($this->config[$itemKey])) {
-            return $this->config[$itemKey];
+    /**
+     * Determine if the given key exists in the provided array.
+     *
+     * @param  \ArrayAccess|array  $array
+     * @param  string|int  $key
+     * @return bool
+     */
+     public static function exists($array, $key)
+     {
+         if ($array instanceof ArrayAccess) {
+             return $array->offsetExists($key);
+         }
+ 
+         return array_key_exists($key, $array);
+     }
+
+     /**
+     * Get an item from an array using "dot" notation.
+     *
+     * @param  \ArrayAccess|array  $array
+     * @param  string  $key
+     * @param  mixed   $default
+     * @return mixed
+     */
+    public function get($key, $default = null){
+        $key = str_replace("mpesa.","",$key);
+        $array = $this->items;
+        if (! static::accessible($array)) {
+            return value($default);
         }
 
-        return $default;
+        if (is_null($key)) {
+            return $array;
+        }
+
+        if (static::exists($array, $key)) {
+            return $array[$key];
+        }
+
+        if (strpos($key, '.') === false) {
+            return $array[$key] ?: value($default);
+        }
+ 
+        foreach (explode('.', $key) as $segment) {
+            if (static::accessible($array) && static::exists($array, $segment)) {
+                $array = $array[$segment];
+            } else {
+                return value($default);
+            }
+        }
+ 
+        return $array;
+    }
+
+    /**
+     * Prepend a value onto an array configuration value.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function prepend($key, $value)
+    {
+        $array = $this->get($key);
+
+        array_unshift($array, $value);
+
+        $this->set($key, $array);
+    }
+
+    
+    /**
+     * Get all of the configuration items for the application.
+     *
+     * @return array
+     */
+    public function all()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Determine if the given configuration option exists.
+     *
+     * @param  string  $key
+     * @return bool
+     */
+    public function offsetExists($key)
+    {
+        return $this->has($key);
+    }
+
+    /**
+     * Get a configuration option.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function offsetGet($key)
+    {
+        return $this->get($key);
+    }
+
+    /**
+     * Set a configuration option.
+     *
+     * @param  string  $key
+     * @param  mixed  $value
+     * @return void
+     */
+    public function offsetSet($key, $value)
+    {
+        $this->set($key, $value);
+    }
+
+    /**
+     * Unset a configuration option.
+     *
+     * @param  string  $key
+     * @return void
+     */
+    public function offsetUnset($key)
+    {
+        $this->set($key, null);
     }
 }
